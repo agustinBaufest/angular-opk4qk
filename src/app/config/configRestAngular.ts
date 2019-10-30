82297ab7bc4a8a2b71849996b76e41f353b5cf44
agustinBaufest <ayanez@baufest.com>
2019-10-30 17:17:02 -0300

    1 file modified

--- src/app/config/configRestAngular.ts
+++ src/app/config/configRestAngular.ts
@@ -1,4 +1,3 @@
 export function RestangularConfigFactory (RestangularProvider) {
-  //RestangularProvider.setBaseUrl('https://my-json-server.typicode.com');
   RestangularProvider.setBaseUrl(' https://apiwebpei.azurewebsites.net/');
 }
